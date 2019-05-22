f211b1af275d90cfed79dbb416ce0a58b8e6effc
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2019-05-22 17:29:38 +0200

    Passport configured for API token auth

--- laravel-api/app/Http/Kernel.php
+++ laravel-api/app/Http/Kernel.php
@@ -19,6 +19,9 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        
+        // Added manually to avoid CORS error on browsers.
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
