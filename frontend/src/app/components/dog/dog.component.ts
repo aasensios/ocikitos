bf4347fe93366177829a6efb3b17d50022be6d91
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2019-07-26 05:16:50 +0200

    re-designed dog form with reactive form technique (not finished)

--- frontend/src/app/components/dog/dog.component.ts
+++ frontend/src/app/components/dog/dog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { map, startWith } from 'rxjs/operators'
 
 import { Dog, Color, Breed } from 'src/app/models/dog'
 import { DogsService } from 'src/app/services/dogs.service'
@@ -7,53 +8,98 @@ import { Sample } from 'src/app/models/sample'
 import { SamplesService } from 'src/app/services/samples.service'
 import { OcikitosResponse } from 'src/app/models/response'
 import { MatSnackBar } from '@angular/material'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-dog',
   templateUrl: './dog.component.html',
-  styleUrls: ['./dog.component.css'],
+  styleUrls: ['./dog.component.scss'],
   providers: [DogsService, SamplesService]
 })
 export class DogComponent implements OnInit {
   @Input() dog: Dog
-  @ViewChild('dogForm', { static: true }) dogForm: HTMLFormElement
+  // @ViewChild("dogForm", { static: true }) dogForm: HTMLFormElement;
   form: FormGroup
   submitted = false
   dogs: Dog[] = []
-  public colors: Color[]
-  public breeds: Breed[]
+  colors: Color[]
+  breeds: Breed[]
+  filteredBreeds: Observable<Breed[]>
   error: string
   success: boolean
   message: string
   editing = false
   sample: Sample
   origins: string[] = ['droppings', 'blood', 'saliva']
-  genders: string[] = ['male', 'female']
+  genders: any[] = [
+    {
+      id: 'male',
+      name: 'Male',
+      icon: 'pets',
+    },
+    {
+      id: 'female',
+      name: 'Female',
+      icon: 'pets',
+    },
+    {
+      id: '',
+      name: 'Unknown',
+      icon: 'cancel',
+    },
+  ]
   selectedValue: string
   response: OcikitosResponse
   SNACKBAR_DURATION_IN_MILISECONDS = 5000
+  CHIP_LENGTH = 15
+
+  toggleEditing() {
+    this.editing = !this.editing
+  }
+
+  /**
+   * https://material.angular.io/components/autocomplete/overview#setting-separate-control-and-display-values
+   */
+  displayFn(breed?: Breed): string | undefined {
+    return breed ? breed.name : undefined
+  }
+
+  private _filter(name: string): Breed[] {
+    const filterValue = name.toLowerCase()
+    return this.breeds.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0)
+  }
 
   constructor(
     private dogsService: DogsService,
     private samplesService: SamplesService,
     private formBuilder: FormBuilder,
     private snackBar: MatSnackBar
-  ) {}
+  ) { }
 
   ngOnInit() {
-    this.dogsService.getColors().subscribe(colors => {
-      this.colors = colors
-    })
-    this.dogsService.getBreeds().subscribe(breeds => {
-      this.breeds = breeds
-    })
+    this.dogsService.getBreeds().subscribe(
+      response => (this.breeds = response.data),
+      errors => console.log(errors),
+      () => {
+        // Filter the breeds autocomplete
+        this.filteredBreeds = this.form.controls.breed.valueChanges.pipe(
+          startWith(''),
+          map(value => typeof value === 'string' ? value : value.name),
+          map(name => name ? this._filter(name) : this.breeds.slice())
+        )
+      }
+    )
+
+    this.dogsService.getColors().subscribe(
+      response => this.colors = response.data
+    )
 
     if (this.dog == null) {
       // this.dog = new Dog();
     }
 
     this.form = this.formBuilder.group({
-      chip: ['', [Validators.required, Validators.minLength(15), Validators.maxLength(15)]],
+      chip: ['', [Validators.required, Validators.minLength(this.CHIP_LENGTH), Validators.maxLength(this.CHIP_LENGTH)]],
       name: ['', Validators.required],
       gender: ['', null],
       breed: ['', null],
@@ -75,7 +121,6 @@ export class DogComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true
-    console.log(this.dog)
 
     // Stop here if form is invalid
     if (this.form.invalid) {
@@ -84,7 +129,7 @@ export class DogComponent implements OnInit {
   }
 
   addDog() {
-    console.log(this.dog)
+    console.log(this.form.value)
 
     this.dogsService.create(this.dog).subscribe(
       // Success
--- frontend/src/app/services/dogs.service.ts
+++ frontend/src/app/services/dogs.service.ts
@@ -1,56 +1,56 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { Dog, Color, Breed } from 'src/app/models/dog'
-import { Observable } from 'rxjs'
-import { API } from './api.constants'
-import { OcikitosResponse } from 'src/app/models/response'
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Dog, Color, Breed } from "src/app/models/dog";
+import { Observable } from "rxjs";
+import { API } from "./api.constants";
+import { OcikitosResponse } from "src/app/models/response";
 
 @Injectable()
 export class DogsService {
   constructor(private http: HttpClient) {}
 
-  getColors(): Observable<Color[]> {
-    return this.http.get<Color[]>(`${API.API_URL}/colors`)
+  getColors(): Observable<OcikitosResponse> {
+    return this.http.get<OcikitosResponse>(`${API.API_URL}/colors`);
   }
 
-  getBreeds(): Observable<Breed[]> {
-    return this.http.get<Breed[]>(`${API.API_URL}/breeds`)
+  getBreeds(): Observable<OcikitosResponse> {
+    return this.http.get<OcikitosResponse>(`${API.API_URL}/breeds`);
   }
 
   getDogs(): Observable<OcikitosResponse> {
-    const url = `${API.API_URL}/dogs`
-    const accessToken = localStorage.getItem('access_token')
-    const options = API.getAuthOptions(accessToken)
-    return this.http.get<OcikitosResponse>(url, options)
+    const url = `${API.API_URL}/dogs`;
+    const accessToken = localStorage.getItem("access_token");
+    const options = API.getAuthOptions(accessToken);
+    return this.http.get<OcikitosResponse>(url, options);
   }
 
   create(dog: Dog): Observable<OcikitosResponse> {
-    const url = `${API.API_URL}/dogs`
-    const body = dog
-    const accessToken = localStorage.getItem('access_token')
-    const options = API.getAuthOptions(accessToken)
-    return this.http.post<OcikitosResponse>(url, body, options)
+    const url = `${API.API_URL}/dogs`;
+    const body = dog;
+    const accessToken = localStorage.getItem("access_token");
+    const options = API.getAuthOptions(accessToken);
+    return this.http.post<OcikitosResponse>(url, body, options);
   }
 
   getOneDog(id: number): Observable<OcikitosResponse> {
-    const url = `${API.API_URL}/dogs/${id}`
-    const accessToken = localStorage.getItem('access_token')
-    const options = API.getAuthOptions(accessToken)
-    return this.http.get<OcikitosResponse>(url, options)
+    const url = `${API.API_URL}/dogs/${id}`;
+    const accessToken = localStorage.getItem("access_token");
+    const options = API.getAuthOptions(accessToken);
+    return this.http.get<OcikitosResponse>(url, options);
   }
 
   update(dog: Dog): Observable<OcikitosResponse> {
-    const url = `${API.API_URL}/dogs/${dog.id}`
-    const body = dog
-    const accessToken = localStorage.getItem('access_token')
-    const options = API.getAuthOptions(accessToken)
-    return this.http.put<OcikitosResponse>(url, body, options)
+    const url = `${API.API_URL}/dogs/${dog.id}`;
+    const body = dog;
+    const accessToken = localStorage.getItem("access_token");
+    const options = API.getAuthOptions(accessToken);
+    return this.http.put<OcikitosResponse>(url, body, options);
   }
 
   delete(dog: Dog): Observable<OcikitosResponse> {
-    const url = `${API.API_URL}/dogs/${dog.id}`
-    const accessToken = localStorage.getItem('access_token')
-    const options = API.getAuthOptions(accessToken)
-    return this.http.delete<OcikitosResponse>(url, options)
+    const url = `${API.API_URL}/dogs/${dog.id}`;
+    const accessToken = localStorage.getItem("access_token");
+    const options = API.getAuthOptions(accessToken);
+    return this.http.delete<OcikitosResponse>(url, options);
   }
 }
